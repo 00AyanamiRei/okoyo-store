7ef345aaaf968664fa8602d7bbe2b86df560bf36
00AyanamiRei <kozlovmax27056@gmail.com>
2024-06-15 21:39:20 +0300

    edited manifest

--- src/main/resources/static/service-worker.js
+++ src/main/resources/static/service-worker.js
@@ -3,39 +3,53 @@ self.addEventListener('install', function(event) {
         caches.open('my-cache').then(function(cache) {
             return cache.addAll([
                 '/',
-                '/index.html',
-                '/shopping-cart.html',
-                '/registration.html',
-                '/checkout.html',
-                '/category.html',
-                '/user/user-main.html',
-                '/user/user-edit.html',
-                '/product/search_product.html',
-                '/product/products_by_category.html',
-                '/product/_fragment.html',
-                '/product/product-page.html',
-                '/error/404.html',
-                '/user/orders.html',
-                '/blocks/breadcrumbs.html',
-                '/blocks/login.html',
-                '/blocks/fragments.html',
-                '/admin-panel.html',
-                '/admin/vendor/vendors.html',
-                '/admin/vendor/vendor_form.html',
-                '/admin/product/products.html',
-                '/admin/product/product_form.html',
-                '/admin/product/product_overview.html',
-                '/admin/product/products_description.html',
-                '/admin/user/users.html',
-                '/admin/user/user_form.html',
-                '/admin/orders/orders.html',
-                '/admin/orders/order_form.html',
-                '/admin/order_basket/order_baskets.html',
-                '/admin/category/categories.html',
-                '/admin/category/category_form.html',
-                '/static/css/main.css',
-                '/static/js/main.js'
+                '/index.html'
+                // '/shopping-cart.html',
+                // '/registration.html',
+                // '/checkout.html',
+                // '/category.html',
+                // '/user/user-main.html',
+                // '/user/user-edit.html',
+                // '/product/search_product.html',
+                // '/product/products_by_category.html',
+                // '/product/_fragment.html',
+                // '/product/product-page.html',
+                // '/error/404.html',
+                // '/user/orders.html',
+                // '/blocks/breadcrumbs.html',
+                // '/blocks/login.html',
+                // '/blocks/fragments.html',
+                // '/admin-panel.html',
+                // '/admin/vendor/vendors.html',
+                // '/admin/vendor/vendor_form.html',
+                // '/admin/product/products.html',
+                // '/admin/product/product_form.html',
+                // '/admin/product/product_overview.html',
+                // '/admin/product/products_description.html',
+                // '/admin/user/users.html',
+                // '/admin/user/user_form.html',
+                // '/admin/orders/orders.html',
+                // '/admin/orders/order_form.html',
+                // '/admin/order_basket/order_baskets.html',
+                // '/admin/category/categories.html',
+                // '/admin/category/category_form.html',
+                // '/static/css/main.css',
+                // '/static/js/main.js'
             ]);
+        })
+    );
+});
+
+self.addEventListener('install', function(event) {
+    event.waitUntil(
+        caches.open('my-cache').then(function(cache) {
+            return Promise.all(
+                filesToCache.map(function(file) {
+                    return cache.add(file).catch(function(error) {
+                        console.error('Failed to cache', file, error);
+                    });
+                })
+            );
         })
     );
 });
