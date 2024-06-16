43e3e965399e80f47d62598510bd6bc29a601ca8
00AyanamiRei <kozlovmax27056@gmail.com>
2024-06-16 15:22:10 +0300

    edited manifest

--- target/classes/static/service-worker.js
+++ target/classes/static/service-worker.js
@@ -19,58 +19,58 @@
 
 // ************************************************ 2 TYPE
 
-// const CACHE = "pwabuilder-offline-page";
-//
-// importScripts('https://storage.googleapis.com/workbox-cdn/releases/5.1.2/workbox-sw.js');
-//
-// // TODO: replace the following with the correct offline fallback page i.e.: const offlineFallbackPage = "offline.html";
-// const offlineFallbackPage = "index.html";
-//
-// self.addEventListener("message", (event) => {
-//     if (event.data && event.data.type === "SKIP_WAITING") {
-//         self.skipWaiting();
-//     }
-// });
-//
-// self.addEventListener('install', async (event) => {
-//     event.waitUntil(
-//         caches.open(CACHE)
-//             .then((cache) => cache.add(offlineFallbackPage))
-//     );
-// });
-//
-// if (workbox.navigationPreload.isSupported()) {
-//     workbox.navigationPreload.enable();
-// }
-//
-// workbox.routing.registerRoute(
-//     new RegExp('/*'),
-//     new workbox.strategies.StaleWhileRevalidate({
-//         cacheName: CACHE
-//     })
-// );
-//
-// self.addEventListener('fetch', (event) => {
-//     if (event.request.mode === 'navigate') {
-//         event.respondWith((async () => {
-//             try {
-//                 const preloadResp = await event.preloadResponse;
-//
-//                 if (preloadResp) {
-//                     return preloadResp;
-//                 }
-//
-//                 const networkResp = await fetch(event.request);
-//                 return networkResp;
-//             } catch (error) {
-//
-//                 const cache = await caches.open(CACHE);
-//                 const cachedResp = await cache.match(offlineFallbackPage);
-//                 return cachedResp;
-//             }
-//         })());
-//     }
-// });
+const CACHE = "pwabuilder-offline-page";
+
+importScripts('https://storage.googleapis.com/workbox-cdn/releases/5.1.2/workbox-sw.js');
+
+// TODO: replace the following with the correct offline fallback page i.e.: const offlineFallbackPage = "offline.html";
+const offlineFallbackPage = "/";
+
+self.addEventListener("message", (event) => {
+    if (event.data && event.data.type === "SKIP_WAITING") {
+        self.skipWaiting();
+    }
+});
+
+self.addEventListener('install', async (event) => {
+    event.waitUntil(
+        caches.open(CACHE)
+            .then((cache) => cache.add(offlineFallbackPage))
+    );
+});
+
+if (workbox.navigationPreload.isSupported()) {
+    workbox.navigationPreload.enable();
+}
+
+workbox.routing.registerRoute(
+    new RegExp('/*'),
+    new workbox.strategies.StaleWhileRevalidate({
+        cacheName: CACHE
+    })
+);
+
+self.addEventListener('fetch', (event) => {
+    if (event.request.mode === 'navigate') {
+        event.respondWith((async () => {
+            try {
+                const preloadResp = await event.preloadResponse;
+
+                if (preloadResp) {
+                    return preloadResp;
+                }
+
+                const networkResp = await fetch(event.request);
+                return networkResp;
+            } catch (error) {
+
+                const cache = await caches.open(CACHE);
+                const cachedResp = await cache.match(offlineFallbackPage);
+                return cachedResp;
+            }
+        })());
+    }
+});
 
 // ************************************************ 3 TYPE
 
@@ -119,55 +119,55 @@
 // ************************************************ 4  TYPE
 
 // This is the service worker with the combined offline experience (Offline page + Offline copy of pages)
-
-const CACHE = "pwabuilder-offline-page";
-
-importScripts('https://storage.googleapis.com/workbox-cdn/releases/5.1.2/workbox-sw.js');
-
-// TODO: replace the following with the correct offline fallback page i.e.: const offlineFallbackPage = "offline.html";
-const offlineFallbackPage = "/";
-
-self.addEventListener("message", (event) => {
-    if (event.data && event.data.type === "SKIP_WAITING") {
-        self.skipWaiting();
-    }
-});
-
-self.addEventListener('install', async (event) => {
-    event.waitUntil(
-        caches.open(CACHE)
-            .then((cache) => cache.add(offlineFallbackPage))
-    );
-});
-
-if (workbox.navigationPreload.isSupported()) {
-    workbox.navigationPreload.enable();
-}
-
-workbox.routing.registerRoute(
-    new RegExp('/*'),
-    new workbox.strategies.StaleWhileRevalidate({
-        cacheName: CACHE
-    })
-);
-
-self.addEventListener('fetch', (event) => {
-    event.respondWith((async () => {
-        try {
-            const preloadResp = await event.preloadResponse;
-
-            if (preloadResp) {
-                return preloadResp;
-            }
-
-            const networkResp = await fetch(event.request);
-            return networkResp;
-        } catch (error) {
-            console.log('Fetch failed for:', event.request.url); // Log the request that is causing the error
-            //test
-            const cache = await caches.open(CACHE);
-            const cachedResp = await cache.match(offlineFallbackPage);
-            return cachedResp;
-        }
-    })());
-});
+//
+// const CACHE = "pwabuilder-offline-page";
+//
+// importScripts('https://storage.googleapis.com/workbox-cdn/releases/5.1.2/workbox-sw.js');
+//
+// // TODO: replace the following with the correct offline fallback page i.e.: const offlineFallbackPage = "offline.html";
+// const offlineFallbackPage = "/";
+//
+// self.addEventListener("message", (event) => {
+//     if (event.data && event.data.type === "SKIP_WAITING") {
+//         self.skipWaiting();
+//     }
+// });
+//
+// self.addEventListener('install', async (event) => {
+//     event.waitUntil(
+//         caches.open(CACHE)
+//             .then((cache) => cache.add(offlineFallbackPage))
+//     );
+// });
+//
+// if (workbox.navigationPreload.isSupported()) {
+//     workbox.navigationPreload.enable();
+// }
+//
+// workbox.routing.registerRoute(
+//     new RegExp('/*'),
+//     new workbox.strategies.StaleWhileRevalidate({
+//         cacheName: CACHE
+//     })
+// );
+//
+// self.addEventListener('fetch', (event) => {
+//     event.respondWith((async () => {
+//         try {
+//             const preloadResp = await event.preloadResponse;
+//
+//             if (preloadResp) {
+//                 return preloadResp;
+//             }
+//
+//             const networkResp = await fetch(event.request);
+//             return networkResp;
+//         } catch (error) {
+//             console.log('Fetch failed for:', event.request.url); // Log the request that is causing the error
+//             //test
+//             const cache = await caches.open(CACHE);
+//             const cachedResp = await cache.match(offlineFallbackPage);
+//             return cachedResp;
+//         }
+//     })());
+// });
