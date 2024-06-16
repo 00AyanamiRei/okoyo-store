915809b21f7f047533c4ef9e6fe2b03d7bec076d
00AyanamiRei <kozlovmax27056@gmail.com>
2024-06-16 11:55:46 +0300

    edited manifest

--- src/main/resources/static/service-worker.js
+++ src/main/resources/static/service-worker.js
@@ -1,40 +1,57 @@
-const CACHE_NAME = 'cool-cache';
+const CACHE = "pwabuilder-offline";
 
-// Add whichever assets you want to pre-cache here:
-const PRECACHE_ASSETS = [
-    '/',
-    '/index.html',
-    '/static/css/main.css',
-    '/static/js/main.js',
-    '/static/assets/',
-    '/static/',
-    q   ``
-]
+importScripts('https://storage.googleapis.com/workbox-cdn/releases/5.1.2/workbox-sw.js');
 
-// Listener for the install event - pre-caches our assets list on service worker install.
-self.addEventListener('install', event => {
-    event.waitUntil((async () => {
-        const cache = await caches.open(CACHE_NAME);
-        cache.addAll(PRECACHE_ASSETS);
-    })());
+self.addEventListener("message", (event) => {
+    if (event.data && event.data.type === "SKIP_WAITING") {
+        self.skipWaiting();
+    }
 });
 
-self.addEventListener('activate', event => {
-    event.waitUntil(self.clients.claim());
-});
+workbox.routing.registerRoute(
+    new RegExp('/*'),
+    new workbox.strategies.StaleWhileRevalidate({
+        cacheName: CACHE
+    })
+);
 
-self.addEventListener('fetch', event => {
-  event.respondWith(
-    caches.match(event.request)
-      .then(response => {
-        // Cache hit - return the response from the cached version
-        if (response) {
-          return response;
-        }
-
-        // Not in cache - return the result from the network
-        return fetch(event.request);
-      }
-    )
-  );
-});
+// const CACHE_NAME = 'cool-cache';
+//
+// // Add whichever assets you want to pre-cache here:
+// const PRECACHE_ASSETS = [
+//     '/',
+//     '/index.html',
+//     '/static/css/main.css',
+//     '/static/js/main.js',
+//     '/static/assets/',
+//     '/static/',
+//     q   ``
+// ]
+//
+// // Listener for the install event - pre-caches our assets list on service worker install.
+// self.addEventListener('install', event => {
+//     event.waitUntil((async () => {
+//         const cache = await caches.open(CACHE_NAME);
+//         cache.addAll(PRECACHE_ASSETS);
+//     })());
+// });
+//
+// self.addEventListener('activate', event => {
+//     event.waitUntil(self.clients.claim());
+// });
+//
+// self.addEventListener('fetch', event => {
+//   event.respondWith(
+//     caches.match(event.request)
+//       .then(response => {
+//         // Cache hit - return the response from the cached version
+//         if (response) {
+//           return response;
+//         }
+//
+//         // Not in cache - return the result from the network
+//         return fetch(event.request);
+//       }
+//     )
+//   );
+// });
