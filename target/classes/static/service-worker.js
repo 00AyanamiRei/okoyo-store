67beec0f0f0b1efa05164a1cb2ed977059f6a91d
00AyanamiRei <kozlovmax27056@gmail.com>
2024-06-16 03:26:33 +0300

    edited manifest

--- target/classes/static/service-worker.js
+++ target/classes/static/service-worker.js
@@ -1,80 +1,42 @@
-self.addEventListener('install', function(event) {
-    event.waitUntil(
-        caches.open('my-cache').then(function(cache) {
-            return cache.addAll([
-                '/',
-                '/index.html'
-                // '/shopping-cart.html',
-                // '/registration.html',
-                // '/checkout.html',
-                // '/category.html',
-                // '/user/user-main.html',
-                // '/user/user-edit.html',
-                // '/product/search_product.html',
-                // '/product/products_by_category.html',
-                // '/product/_fragment.html',
-                // '/product/product-page.html',
-                // '/error/404.html',
-                // '/user/orders.html',
-                // '/blocks/breadcrumbs.html',
-                // '/blocks/login.html',
-                // '/blocks/fragments.html',
-                // '/admin-panel.html',
-                // '/admin/vendor/vendors.html',
-                // '/admin/vendor/vendor_form.html',
-                // '/admin/product/products.html',
-                // '/admin/product/product_form.html',
-                // '/admin/product/product_overview.html',
-                // '/admin/product/products_description.html',
-                // '/admin/user/users.html',
-                // '/admin/user/user_form.html',
-                // '/admin/orders/orders.html',
-                // '/admin/orders/order_form.html',
-                // '/admin/order_basket/order_baskets.html',
-                // '/admin/category/categories.html',
-                // '/admin/category/category_form.html',
-                // '/static/css/main.css',
-                // '/static/js/main.js'
-            ]);
-        })
-    );
+const CACHE_NAME = 'cool-cache';
+
+// Add whichever assets you want to pre-cache here:
+const PRECACHE_ASSETS = [
+    '/',
+    '/index.html',
+    '/static/css/main.css',
+    '/static/js/main.js',
+    '/static/assets/',
+    '/static/',
+    '/templates/'
+]
+
+// Listener for the install event - pre-caches our assets list on service worker install.
+self.addEventListener('install', event => {
+    event.waitUntil((async () => {
+        const cache = await caches.open(CACHE_NAME);
+        cache.addAll(PRECACHE_ASSETS);
+    })());
 });
 
-self.addEventListener('install', function(event) {
-    event.waitUntil(
-        caches.open('my-cache').then(function(cache) {
-            return Promise.all(
-                filesToCache.map(function(file) {
-                    return cache.add(file).catch(function(error) {
-                        console.error('Failed to cache', file, error);
-                    });
-                })
-            );
-        })
-    );
+self.addEventListener('activate', event => {
+    event.waitUntil(self.clients.claim());
 });
 
-self.addEventListener('fetch', function(event) {
-    event.respondWith(
-        caches.match(event.request).then(function(response) {
-            return response || fetch(event.request);
-        })
-    );
-});
+self.addEventListener('fetch', event => {
+    event.respondWith(async () => {
+        const cache = await caches.open(CACHE_NAME);
 
+        // match the request to our cache
+        const cachedResponse = await cache.match(event.request);
 
-self.addEventListener('activate', function(event) {
-    var cacheWhitelist = ['my-cache'];
-
-    event.waitUntil(
-        caches.keys().then(function(cacheNames) {
-            return Promise.all(
-                cacheNames.map(function(cacheName) {
-                    if (cacheWhitelist.indexOf(cacheName) === -1) {
-                        return caches.delete(cacheName);
-                    }
-                })
-            );
-        })
-    );
+        // check if we got a valid response
+        if (cachedResponse !== undefined) {
+            // Cache hit, return the resource
+            return cachedResponse;
+        } else {
+            // Otherwise, go to the network
+            return fetch(event.request)
+        };
+    });
 });
