67029dc6bf267752de94d06e1344ba8ff8cfbbfc
00AyanamiRei <kozlovmax27056@gmail.com>
2024-06-16 14:56:03 +0300

    edited manifest

--- target/classes/static/service-worker.js
+++ target/classes/static/service-worker.js
@@ -152,23 +152,21 @@ workbox.routing.registerRoute(
 );
 
 self.addEventListener('fetch', (event) => {
-    if (event.request.mode === 'navigate') {
-        event.respondWith((async () => {
-            try {
-                const preloadResp = await event.preloadResponse;
+    event.respondWith((async () => {
+        try {
+            const preloadResp = await event.preloadResponse;
 
-                if (preloadResp) {
-                    return preloadResp;
-                }
-
-                const networkResp = await fetch(event.request);
-                return networkResp;
-            } catch (error) {
-
-                const cache = await caches.open(CACHE);
-                const cachedResp = await cache.match(offlineFallbackPage);
-                return cachedResp;
+            if (preloadResp) {
+                return preloadResp;
             }
-        })());
-    }
+
+            const networkResp = await fetch(event.request);
+            return networkResp;
+        } catch (error) {
+            console.log('Fetch failed for:', event.request.url); // Log the request that is causing the error
+            const cache = await caches.open(CACHE);
+            const cachedResp = await cache.match(offlineFallbackPage);
+            return cachedResp;
+        }
+    })());
 });
